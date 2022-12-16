75ca43bfced354e9efee86a51d28db821d44a6fe
Julia H <108383094+bliptie@users.noreply.github.com>
2022-12-16 18:10:33 +1300

    Create writeDatabase.php

---
+++ writeDatabase.php
@@ -0,0 +1,21 @@
+<?php
+    require 'vendor/autoload.php';
+    use Aws\DynamoDb\DynamoDbClient;
+
+    $client = DynamoDbClient::factory(array(
+        'region' => 'us-east-1',
+        'version' => 'latest'
+        
+      ));
+
+    $tableName = "CotissDB";
+
+    $response = $client->putItem(array(
+      'TableName' => $tableName,
+      'Item' => $client->formatAttributes(array(
+        'message' => $_POST['message']
+        ))
+      ));
+
+   header('Location: createDatabase.php');
+?>
